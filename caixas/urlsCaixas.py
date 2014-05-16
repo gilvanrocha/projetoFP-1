e6c1b51a88877f3bb9738fd9fd9fcd25f2df3cd9
GILVANROCHA <gilvanrocha@gmail.com>
2014-05-16 18:45:20 -0300

    ajustando caixas

--- caixas/urlsCaixas.py
+++ caixas/urlsCaixas.py
@@ -5,7 +5,7 @@ https://www.facebook.com/groups/pythonmania/
 
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('caixas.views'),
+urlpatterns = patterns('caixas.views',
     url(r'^adicionar/$', 'caixaAdicionar'),
     url(r'^editar/(?P<pk>\d+)/$', 'caixaEditar'),
     url(r'^salvar/$', 'caixaSalvar'),
